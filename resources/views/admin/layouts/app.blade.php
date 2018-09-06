4439af882244463e653efe327cb4525bcb9cde0a
Denis Romanov <lampard.ks@gmail.com>
2018-09-07 02:23:53 +0300

    Update VPN Admin (Laravel)

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -17,9 +17,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">    
 </head>
 <body>
     <div id="app">
@@ -60,7 +58,6 @@
     </div>
     
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    
+    <script src="{{ asset('js/app.js') }}" defer></script>    
 </body>
 </html>
